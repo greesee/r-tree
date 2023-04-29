752cdc52a7f767699f6539630bff3b00dfa3e891
AAK <anishkgkar@gmail.com>
2023-04-30 00:23:24 +0530

    formatted output

--- rtree.c
+++ rtree.c
@@ -1,12 +1,26 @@
+/*  Implemented by:
+    Anish Ashish Kasegaonkar
+    Abhinav Sahay
+    Jay Prakash Mundhra
+    Harsh Pokarna
+    Srijan Khatri
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 
-#define m 2                          /*minimum children; sizeof child index*/
-#define M 4                          /*maximum children; sizeof child index*/
+#define m 2                          /* Minimum children */
+#define M 4                          /* Maximum children */
 #define INF 1e9
 
+// ************************************************************************************
+//                              STRUCTS AND OBJECT CREATION
+
+/*  Node of RTree, that contains a list of children nodes and their respective MBRs.
+    Contains MBR of all children nodes' MBRs for efficient sorting.
+*/
 typedef struct node {
     int MBR[M][4];
     struct node* children[M];
@@ -15,10 +29,14 @@ typedef struct node {
     int nodeMBR[4];
 } node;
 
+/*  RTree that contains a pointer to the root node.
+*/
 typedef struct rtree {
-    node *root;
+    node* root;
 } rtree;
 
+/*  Construct a new node.
+*/
 node* new_node(){
     node* n = (node *)malloc(sizeof(node));
     for(int i = 0; i < M; i++) {
@@ -36,23 +54,21 @@ node* new_node(){
     return n;
 }
 
+/*  Construct a new tree.
+*/
 rtree* new_tree(){
     rtree* t = (rtree*)malloc(sizeof(rtree));
     t->root = NULL;
     return t;
 }
 
-int* new_MBR(){
-    int* MBR = (int*)malloc(sizeof(int) * 4);
-    MBR[0] = INF;
-    MBR[1] = INF;
-    MBR[2] = -INF;
-    MBR[3] = -INF;
-    return MBR;
-}
-
 // ************************************************************************************
+//                                  MERGE SORT
 
+/*  Comparator for merge sort
+    Compares based on sum of (minX + maxX) or (minY + maxY) based on sortOnInd, which is essentially comparing based on midpoint of MBR.
+    Checks with respect to opposite coordinate in equality condition.
+*/
 bool comp(rtree** A, rtree** B, int i, int j, int sortOnInd) {
     if ((A[i]->root->nodeMBR[sortOnInd] + A[i]->root->nodeMBR[sortOnInd + 2]) == (B[j]->root->nodeMBR[sortOnInd] + B[j]->root->nodeMBR[sortOnInd + 2])) {
         return (A[i]->root->nodeMBR[(sortOnInd) ^ 1] + A[i]->root->nodeMBR[(sortOnInd + 2) ^ 1]) < (B[j]->root->nodeMBR[(sortOnInd) ^ 1] + B[j]->root->nodeMBR[(sortOnInd + 2) ^ 1]);
@@ -60,43 +76,44 @@ bool comp(rtree** A, rtree** B, int i, int j, int sortOnInd) {
     return (A[i]->root->nodeMBR[sortOnInd] + A[i]->root->nodeMBR[sortOnInd + 2]) < (B[j]->root->nodeMBR[sortOnInd] + B[j]->root->nodeMBR[sortOnInd + 2]);
 }
 
+/*  Minimum of two integers
+*/
 int min(int a, int b) {
     if(a < b) return a;
     return b;
 }
 
+/*  Maximum of two integers
+*/
 int max(int a, int b) {
     if(a < b) return b;
     return a;
 }
 
+/*  Iteratively merging two list of rtree pointers (same as standard merge sort).
+    Shallow copy used since we do not alter the tree underlying the root node.
+*/
 void mergeAuxIter(rtree** L1, int s1, int e1, rtree** L2, int s2, int e2, rtree** L3, int s3, int e3, int sortOnInd) {
     int i, j, k;
     i = s1; j = s2; k = s3;
     while (i <= e1 && j <= e2) {
         if (comp(L1, L2, i, j, sortOnInd)) {
-            L3[k]->root = L1[i]->root;
-            k++;
-            i++;
+            L3[k++]->root = L1[i++]->root;
         }
         else {
-            L3[k]->root = L2[j]->root;
-            k++;
-            j++;
+            L3[k++]->root = L2[j++]->root;
         }
     }
     while (i <= e1) {
-        L3[k]->root = L1[i]->root;
-        k++;
-        i++;
+        L3[k++]->root = L1[i++]->root;
     }
     while (j <= e2) {
-        L3[k]->root = L2[j]->root;
-        k++;
-        j++;
+        L3[k++]->root = L2[j++]->root;
     }
 }
 
+/*  Creates a temporary list of rtree* for merging operation, frees it once utilized (same as standard merge sort).
+*/
 void merge(rtree** A, int s, int mid, int e, int sortOnInd) {
     rtree** C = (rtree**)malloc(sizeof(rtree*) * (e - s + 1));
     for(int i = 0; i < e - s + 1; i++) {
@@ -112,6 +129,8 @@ void merge(rtree** A, int s, int mid, int e, int sortOnInd) {
     free(C); 
 }
 
+/*  Standard merge sort function that recursively solves smaller arrays, uses it to solve a larger array of rtree*
+*/
 void mergeSort(rtree** A, int st, int en, int sortOnInd) {
     if(en <= st) return;
     int mid = st + (en - st)/2;
@@ -121,38 +140,69 @@ void mergeSort(rtree** A, int st, int en, int sortOnInd) {
 }
 
 // ************************************************************************************
+//                          PRE-ORDER TRAVERSAL
 
-void dfs(node* root, int level) {
-    printf("\n\nNode MBR (of all children) = ");
-    for(int i = 0; i < 4; i++) {
-        printf("%d, ", root->nodeMBR[i]);
-    }
-    printf("\nDFS Level: %d\nNode children = ", level);
-    for(int i = 0; i < M; i++) {
-        printf("\n");
-        for(int j = 0; j < 4; j++) {
-            printf("%d, ", root->MBR[i][j]);
+/*  Pre-order traversal (DFS) that prints current node before printing its children.
+    Prints the DFS level, i.e. the depth of the recursion call.
+*/
+void dfs(node* curNode, int level) {
+
+    // Prints the MBR of the entire root node (all its children).
+    if(level == 0) {
+        printf("\nMBR of Root Node: ");
+        for(int i = 0; i < 4; i++) {
+            printf("%d ", curNode->nodeMBR[i]);
         }
     }
+
+    // For each call, prints the DFS depth as well as all children associated with the current node.
+    printf("\n\nDFS Level: %d\nNode children = ", level);
+    for(int i = 0; i < curNode->cnt_keys; i++) {
+        printf("[");
+        for(int j = 0; j < 4; j++) {
+            printf("%d", curNode->MBR[i][j]);
+            if(j != 3) printf(", ");
+        }
+        printf("]");
+        if(i != curNode->cnt_keys - 1) printf(", ");
+    }
+
+    // Recursive DFS call to visit children of the current node.
     for(int i = 0; i < M; i++) {
-        if(root->children[i] != NULL)
-            dfs(root->children[i], level + 1);
+        if(curNode->children[i] != NULL)
+            dfs(curNode->children[i], level + 1);
     }
 }
 
-rtree** buildAux(rtree** curLevel, int numberOfMBRs, int levelNumber){
+// ************************************************************************************
+//                          SORT-TILE-RECURSIVE BUILD
 
+/*  Implementation of Sort-Tile-Recursive algorithm that efficiently bulk-loads data into the RTree.
+    Time complexity = O(N * (log(N))^2), N is the number of points/objects.
+*/
+rtree** buildSTR(rtree** curLevel, int numberOfMBRs, int levelNumber){
+
+    // Calculations for number of slices required, number of nodes in each slice, and total number of nodes in current level.
     int numberOfSlices = ceil(sqrt(numberOfMBRs / (float)M));
     int nodesInSlice = numberOfSlices * M;
     int numberOfNodes = ceil(numberOfMBRs / (float)M);
     
+    // Sorts entire list on basis of X-coordinate of the midpoint.
     mergeSort(curLevel, 0, numberOfMBRs - 1, 0);
+
+    // Sorts each slice on basis of Y-coordinate of the midpoint.
     for(int i = 0; i < numberOfSlices; i++) {
         mergeSort(curLevel, i * nodesInSlice, min((i + 1) * nodesInSlice, numberOfMBRs) - 1, 1);
     }
 
+    // Creates a list of rtree* for the upper level.
+    // Note that level here is not the same as DFS level, as it is from bottom to top.
     rtree** nextLevel = (rtree**)malloc(sizeof(rtree*) * numberOfNodes);
 
+    /*  This loop populates the nextLevel's children's MBRs, which is basically the MBR of the current node (i.e. nodeMBR for each child).
+        Also populates the nodeMBRs for the nextLevel, using these children's MBRs.
+        Uses modular arithmetic to simplify the code.
+    */
     for(int i = 0; i < numberOfMBRs; i++) {
         if(i % M == 0) {
             nextLevel[i / M] = new_tree();
@@ -163,6 +213,7 @@ rtree** buildAux(rtree** curLevel, int numberOfMBRs, int levelNumber){
             if(j < 2) nextLevel[i / M]->root->nodeMBR[j] = min(nextLevel[i / M]->root->nodeMBR[j], curLevel[i]->root->nodeMBR[j]);
             else nextLevel[i / M]->root->nodeMBR[j] = max(nextLevel[i / M]->root->nodeMBR[j], curLevel[i]->root->nodeMBR[j]);
         }
+        // Special case to remove the first level, because the first level has nodes with only one MBR each, and should not be included in final RTree.
         if(levelNumber != 0) {
             curLevel[i]->root->parent = nextLevel[i / M]->root;
             nextLevel[i / M]->root->children[i % M] = curLevel[i]->root;
@@ -170,49 +221,68 @@ rtree** buildAux(rtree** curLevel, int numberOfMBRs, int levelNumber){
         nextLevel[i / M]->root->cnt_keys++;
     }
 
+    // Special case to remove the first level.
     if(levelNumber == 0) {
         for(int i = 0; i < numberOfMBRs; i++) 
             free(curLevel[i]->root);
     }
+
+    // Free up waste memory, list of rtree* as well as the rtree's allocated.
+    // Note that the nodes are not freed, as they are a part of our final RTree
     for(int i = 0; i < numberOfMBRs; i++) 
         free(curLevel[i]);
     free(curLevel);
 
+    // When the number of nodes is 1, return the final list as we have reached the root node.
     if(numberOfNodes == 1) 
         return nextLevel;
 
-    return buildAux(nextLevel, numberOfNodes, levelNumber + 1);
-}
-
-rtree* build(rtree** MBR, int numberOfMBRs) {
-    // check
-    int numberOfLevels = ceil(log10(numberOfMBRs)/log10(M));
-    int numberOfNodes = ceil(numberOfMBRs / (float)M);
-    rtree** listOfSubTrees = buildAux(MBR, numberOfMBRs, 0);
-
-    dfs(listOfSubTrees[0]->root, 0);
-    return NULL;
+    // Recursive call for the upper level of MBRs.
+    return buildSTR(nextLevel, numberOfNodes, levelNumber + 1);
 }
 
 int main() {
-    FILE* f = fopen("large.txt", "r");
+    // Initialises file pointer.
+    FILE* f = fopen("data.txt", "r");
     int listSize = 10;
+
+    // Initial list of rtree* that would store the input, in the root of every tree.
+    // Uses rtree** instead of int** so that the mergeSort function can be reused for the lowest level as well.
     rtree** pointList = (rtree**)malloc(sizeof(rtree*) * listSize);
 
-    int curIndex = 0;
+    // Size of input.
+    int inputSize = 0;
+
+    // Reads until end of file.
     while(!feof(f)) {
-        if(curIndex >= listSize) {
+        
+        // Every time the size of input reaches the list size limit, we reallocate the list with double the memory.
+        // Since this reallocation would be done approximately ceil(log(N)) times, N is input size, there is no efficiency loss.
+        // Similar to "vector" in vector.h library of cpp.
+        if(inputSize >= listSize) {
             listSize *= 2;
             pointList = realloc(pointList, sizeof(rtree*) * listSize);
         }
-        pointList[curIndex] = new_tree();
-        pointList[curIndex]->root = new_node();
-        fscanf(f, "%d %d", &pointList[curIndex]->root->nodeMBR[0], &pointList[curIndex]->root->nodeMBR[1]);
-        pointList[curIndex]->root->nodeMBR[2] = pointList[curIndex]->root->nodeMBR[0];
-        pointList[curIndex]->root->nodeMBR[3] = pointList[curIndex]->root->nodeMBR[1];
-        ++curIndex;
+
+        // Creates a tree, and a root for the tree in which each point is stored in the nodeMBR of a unique node.
+        pointList[inputSize] = new_tree();
+        pointList[inputSize]->root = new_node();
+
+        fscanf(f, "%d %d", &pointList[inputSize]->root->nodeMBR[0], &pointList[inputSize]->root->nodeMBR[1]);
+        pointList[inputSize]->root->nodeMBR[2] = pointList[inputSize]->root->nodeMBR[0];
+        pointList[inputSize]->root->nodeMBR[3] = pointList[inputSize]->root->nodeMBR[1];
+        ++inputSize;
     }
 
+    // Closes file pointer.
     fclose(f);
-    rtree* temp = build(pointList, curIndex);
+
+    // Calls sort-tile-recursive build function which returns a pointer to the tree.
+    rtree** pointerToTree = buildSTR(pointList, inputSize, 0);
+    rtree* finalTree = pointerToTree[0];
+    free(pointerToTree);
+
+    // Runs a pre-order traversal of the tree.
+    dfs(finalTree->root, 0);
+    return 0;
 }
